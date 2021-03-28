7480e751da23009d6460eba7e4fdc0871ca01058
kawintiger <78682512+kawintiger@users.noreply.github.com>
2021-03-28 16:12:53 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace สอบworkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A,B,X,Y;
+            Console.WriteLine("Input Number A");
+            Console.WriteLine("Input Number B");
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            X = A;
+            Y = B;
+
+            while(X!=Y)
+            {
+                if(X<Y)
+                {
+                    X = X + A;
+                }
+                else
+                {
+                    Y = Y + B;
+                }
+                Console.Write(X);
+            } 
+            Console.ReadLine();
+        }
+    }
+}
